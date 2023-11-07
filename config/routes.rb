649e4dc2ed546d0b181800f264b52e4643e14ec4
Wim den Braven <wimdenbraven@persuavis.com>
2023-11-06 16:03:44 -0800

    Add convenience routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Bp3::Hello::Engine::Engine.routes.draw do
   get 'hello/world'
   get 'hello/engine'
+  get 'world', to: 'hello#world'
+  get 'engine', to: 'hello#engine'
 
   root to: 'hello#engine'
 end
